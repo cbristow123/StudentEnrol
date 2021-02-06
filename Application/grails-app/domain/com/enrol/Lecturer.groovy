95f72c760f25dc5054187cb7ba035dece16ca50f
cbristow123 <callumbristow11@gmail.com>
2021-02-06 16:49:16 +0000

    Added two new domains

---
+++ Application/grails-app/domain/com/enrol/Lecturer.groovy
@@ -0,0 +1,21 @@
+package com.enrol
+
+class Lecturer {
+//declaring variables
+	String fullName
+	String post
+	String subject
+	String lecturerEmail
+	String office
+	String bio
+    static constraints = {
+
+//declaring constraints
+	fullName nullable:false, blank:false
+	post nullable:false, blank:false
+	subject nullable:false, blank:false
+	lecturerEmail nullable:false, blank:false, email:true
+	office nullable:false, blank:false
+	bio nullable:false, blank:false, maxSize: 5000, widget:'textarea'
+    }
+}
---
+++ Application/grails-app/domain/com/enrol/Module.groovy
@@ -0,0 +1,21 @@
+package com.enrol
+
+class Module {
+
+//declaring variables
+	String module_title
+	String module_code
+	int credits
+	String lecturer
+	String course
+	String description
+    static constraints = {
+
+//declaring constraints
+	module_title nullable:false, blank:false
+	module_code nullable:false, blank:false
+	credits nullable:false, blank:false, min:20, max:120
+	lecturer nullable:false, blank:false
+	description nullable:false, blank:false, maxSize: 5000, widget:'textarea'
+    }
+}
